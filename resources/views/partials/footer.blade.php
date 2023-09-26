eb7f8932be5b06b37e5847cea09658ffc1fe9fa4
DarioEsposito89 <darioesposito.work@gmail.com>
2023-09-26 09:48:41 +0200

    caricato sass footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -40,11 +40,11 @@
                 ["name" => "DC power visa"]
             ],
             "listSocial" => [
-                ["img" => "/img/footer-facebook.png"],
-                ["img" => "footer-twitter.png"],
-                ["img" => "footer-youtube.png"],
-                ["img" => "footer-pinterest.png"],
-                ["img" => "footer-periscope.png"]
+                ["img" => "./img/footer-facebook.png"],
+                ["img" => "./img/footer-twitter.png"],
+                ["img" => "./img/footer-youtube.png"],
+                ["img" => "./img/footer-pinterest.png"],
+                ["img" => "./img/footer-periscope.png"]
             ],
             
         ],
